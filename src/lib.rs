1f68080eecd219a1ea8b59a8f907cd293afffae6
Leonardo De Faveri <leonardo.defaveri01@gmail.com>
2024-09-20 15:52:56 +0200

    Fixes to reflect moving of plugin_manager into phosphorus_cor

--- src/lib.rs
+++ src/lib.rs
@@ -1,27 +1,29 @@
 use playlist_manager::Playlist;
+use song::Song;
 
 pub mod playlist_manager;
+pub mod plugin_manager;
 pub mod queue;
 pub mod song;
 
-/// Returnes the name that can represent the provided song
+/// Returnes the name that can represent the provided song. NO EXTENSION!
 ///
 /// # Example
 /// A song called `song` made by `author` will be associated
-/// to a file called `song--author.json`. Everything is lowercase
+/// to a file called `song--author`. Everything is lowercase
 /// and spaces are replaced by `_`.
 pub fn file_name_from_song(song: &song::Song) -> String {
     let details = song.details();
     let name = details.name();
     let artist = details.artist().unwrap_or("");
-    format!("{}--{}.json", name, artist)
+    format!("{}--{}", name, artist)
         .to_lowercase()
         .replace(" ", "_")
 }
 
 /// As `file_name_from_song` by takes in input only name and artist of that song.
 pub fn file_name_from_basics(song_name: &str, song_artist: &str) -> String {
-    format!("{}--{}.json", song_name, song_artist)
+    format!("{}--{}", song_name, song_artist)
         .to_lowercase()
         .replace(" ", "_")
 }
@@ -36,4 +38,18 @@ pub fn file_name_from_playlist(playlist: &Playlist) -> String {
     format!("{}.json", playlist.name())
         .to_lowercase()
         .replace(" ", "_")
+}
+
+/// This enum should be used to pass around the app tranking information for
+/// downloads.
+pub enum TrackInfo {
+    /// The download for the song has been registered, but not yet started
+    New(Song),
+    /// The download for the song is started
+    Started(Song),
+    /// The download for the song has made progresses
+    Progress(Song, f32),
+    /// The download for the song is terminated. Terminated should mean that the
+    /// file is totally downloaded and the raw media file is ready to be read
+    Finished(Song),
 }
